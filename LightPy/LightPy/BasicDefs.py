cf31b9aaffc816a61ec3dc9460e4253ce93a6ad7
timmyred456 <timmy.anderson.aol.com@gmail.com>
2021-03-13 06:54:41 -0500

    Added Numby

--- LightPy/LightPy/BasicDefs.py
+++ LightPy/LightPy/BasicDefs.py
@@ -1,20 +1,26 @@
-import math
+import math, numba
 from LightPy.DataTypes import *
+from numba import jit
 
 # No idea what this one does lmao
+@jit(nopython=True)
 def ccw(A,B,C):
     return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
 
 # Return true if line segments AB and CD intersect
+@jit(nopython=True)
 def intersect(A:tuple,B:tuple,C:tuple,D:tuple):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 # Return Dist from TwoPoints
+@jit(nopython=True)
 def dist(point1:tuple, point2:tuple):
     return math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
+@jit(nopython=True)
 def clamp(value, Min, Max):
     return max(Min, min(value, Max))
+
 
 def IntersectAtPoint(Point1:vector2, Point2:vector2, Walls):
     result = False
--- LightPy/main.py
+++ LightPy/main.py
@@ -1,111 +1,78 @@
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame, math
-import threading, multiprocessing
-from multiprocessing import Process
+import pygame, math, threading, numba
+from numba import jit
 from LightPy import *
 
-global FinalSheet
+pygame.init()
 
-FinalSheet = []
+size = [600,600]
 
-def RenderThread1(Sizex,Sizey,Lights,Walls,Q,ID):
-    Size = (Sizex,Sizey)
-    print("StartCalc")
-    FinalList = CalcBrightSheet(Lights,Walls,Size)
-    Q.put(1)
-    print("Finished, POG")
+ 
+Lights = []
+Walls = []
 
-def RenderBrights(RenderBuffer, size, Lights, Walls):
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("LightPy V1")
 
-    ThreadAmmount = 32
-    ThreadStep = math.floor(size[1] / ThreadAmmount)
-    Threads = []
-    Q = multiprocessing.Queue()
+render = pygame.surface.Surface(size)
 
-    FinalSheet = []
+Quality = 5
 
-    for x in range(ThreadAmmount):
-        Threads.append(Process(target=RenderThread1, args=(size[0], ThreadStep*(x+1),Lights,Walls,Q,x)))
-
-    for x in range(0, len(Threads)):
-        Threads[x].start()
-
-    for x in range(0, len(Threads)):
-        print("Joining Thread", x)
-        Threads[x].join()
-        print("get")
-        Q.get_nowait()
-        print("got")
-
-
-    print("All Joined")
-
-    print(FinalSheet)
-
-    for y in range(size[1]):
-        for x in range(size[0]):
-            brt = FinalSheet[y][x]
-            pygame.draw.rect(RenderBuffer, (brt,brt,brt), ((x,y), (1,1)))
+def RenderBrights():
+    Brights = CalcBrightSheet(Lights, Walls, size)
+    for y in range(0,size[1]):
+        for x in range(0,size[0]):
+            brtFn = (Brights[y][x], Brights[y][x], Brights[y][x])
+            pygame.draw.rect(render, brtFn, ((x,y),(1,1)))
 
 
 
 
-if __name__ == "__main__":
-    pygame.init()
-    size = [600,600]
-    Lights = []
-    Walls = []
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("LightPy V1")
-    render = pygame.surface.Surface(size)
-    renderBuffer = pygame.surface.Surface(size)
-    Quality = 5
-    done = False
-    clock = pygame.time.Clock()
-    WallCords = []
 
-    while not done:
-        screen.blit(render,(0,0))
-        xy = pygame.mouse.get_pos()
-    
-        # This limits the while loop to a max of x times per second.
-        clock.tick()
-        # Leave this out and we will use all CPU we can.
-    
-    
-        if len(WallCords) > 0 and len(WallCords) < 2:
-            pygame.draw.line(screen, (155,155,255), (WallCords[0][0], WallCords[0][1]), xy)
-        elif len(WallCords) == 2:
-            Walls.append(WallObject(vector2(WallCords[0][0], WallCords[0][1]), vector2(WallCords[1][0], WallCords[1][1])))
-            WallCords = []
-    
-    
-        for x in range(0,len(Walls)):
-            pygame.draw.line(screen, (255,255,255), Walls[x].StartPoint.AsTuple(), Walls[x].EndPoint.AsTuple())
-    
-        for x in range(0,len(Lights)):
-            pygame.draw.rect(screen, (255,155,255), ((Lights[x].pos.x - 5, Lights[x].pos.y - 5), (15,15)))
-    
-    
-    
-        for event in pygame.event.get(): # User did something
-            if event.type == pygame.QUIT: # If user clicked close
-                done=True # Flag that we are done so we exit this loop
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == pygame.BUTTON_LEFT:
-                    Lights.append(LightObject(vector2(xy[0], xy[1]), 8))
-                    print(xy)
-                if event.button == pygame.BUTTON_RIGHT:
-                    WallCords.append(xy)
-                    print(xy)
-            if event.type == pygame.KEYUP:
-                if event.key== pygame.K_RETURN:
-                    print(Lights)
-                    RenderBrights(renderBuffer, size, Lights, Walls)
-    
-        #pygame.draw.rect(screen, (255,255,255), ((x,y),(1,1)))
-        #pygame.draw.rect(screen, (Col3), ((PosX,PosY),(WidthX,HeightY)))
-    
-        pygame.display.update()
-        pygame.draw.rect(screen, (0,0,0), ((0,0), (size[0],size[1])))
+done = False
+clock = pygame.time.Clock()
+WallCords = []
+
+while not done:
+    xy = pygame.mouse.get_pos()
+    screen.blit(render,(0,0))
+
+    # This limits the while loop to a max of x times per second.
+    clock.tick()
+    # Leave this out and we will use all CPU we can.
+
+
+    if len(WallCords) > 0 and len(WallCords) < 2:
+        pygame.draw.line(screen, (155,155,255), (WallCords[0][0], WallCords[0][1]), xy)
+    elif len(WallCords) == 2:
+        Walls.append(WallObject(vector2(WallCords[0][0], WallCords[0][1]), vector2(WallCords[1][0], WallCords[1][1])))
+        WallCords = []
+
+
+    for x in range(0,len(Walls)):
+        pygame.draw.line(screen, (255,255,255), Walls[x].StartPoint.AsTuple(), Walls[x].EndPoint.AsTuple())
+
+    for x in range(0,len(Lights)):
+        pygame.draw.rect(screen, (255,155,255), ((Lights[x].pos.x - 5, Lights[x].pos.y - 5), (15,15)))
+
+
+
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == pygame.BUTTON_LEFT:
+                Lights.append(LightObject(vector2(xy[0], xy[1]), 8))
+                print(xy)
+            if event.button == pygame.BUTTON_RIGHT:
+                WallCords.append(xy)
+                print(xy)
+        if event.type == pygame.KEYUP:
+            if event.key== pygame.K_RETURN:
+                print(Lights)
+                RenderBrights()
+
+    #pygame.draw.rect(screen, (255,255,255), ((x,y),(1,1)))
+    #pygame.draw.rect(screen, (Col3), ((PosX,PosY),(WidthX,HeightY)))
+
+    pygame.display.update()
+    pygame.draw.rect(screen, (0,0,0), ((0,0), (size[0],size[1])))
