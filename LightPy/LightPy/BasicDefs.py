843c111d73419402b8f2593ef576f1fba4ae7651
timmyred456 <timmy.anderson.aol.com@gmail.com>
2021-03-27 09:36:49 -0400

    Changes Descriptions

--- LightPy/LightPy/BasicDefs.py
+++ LightPy/LightPy/BasicDefs.py
@@ -6,9 +6,9 @@ from numba import jit
 #  -Really good for Math heavy operations, when you put it before a function, run the function once to compile
 #  -Does not work for custom classes, especialy when iterating alot with them
 
-# No idea what this one does lmao
+# No idea what this one below does lmao
 @jit(nopython=True)
-def ccw(A,B,C):
+def ccw(A,B,C): 
     return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
 
 # Return true if line segments AB and CD intersect
@@ -21,10 +21,12 @@ def intersect(A:tuple,B:tuple,C:tuple,D:tuple):
 def dist(point1:tuple, point2:tuple):
     return math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
+# Clamp Value between Min, and Max
 @jit(nopython=True)
 def clamp(value, Min, Max):
     return max(Min, min(value, Max))
 
+#Loops through all walls, if they intersect it returns the result
 def IntersectAtPoint(Point1:tuple, Point2:tuple, Walls):
     result = False
     for x in range(0,len(Walls)):
